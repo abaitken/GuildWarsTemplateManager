2d4a0a8b6a6e03d085ec88b1faabe517bdc0f5aa
Alex Boyne-Aitken <abaitken@live.co.uk>
2020-03-10 17:18:32 +0000

    Replaced GlobalAssemblyInfo with CompanyAssemblyInfo

---
+++ Source/Include/CompanyAssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Tempered Software")]
+[assembly: AssemblyCopyright("Copyright 2012 Tempered Software")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
